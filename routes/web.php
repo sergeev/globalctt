203ced408a75e2a56dc93ae8b932598d0ed4381d
sergeev <v.sergeev.m@gmail.com>
2020-07-31 16:14:20 +0700

    Add Users clear page & add users route to web

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function(){
+	Route::resource('/users', 'UserController', ['except' => ['show', 'create', 'store']]);
+});
