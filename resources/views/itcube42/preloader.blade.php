f72bdb060c54f0d1c16ad072a68c2854e9280a44
sergeev <v.sergeev.m@gmail.com>
2020-12-01 09:49:40 +0700

    Remove itcube in master, only kvantorium branch this

--- resources/views/itcube42/preloader.blade.php
+++
@@ -1,6 +0,0 @@
- <!-- JavaScript Libraries -->
-  <script src="{{ URL::asset('itcube/js/jquery-3.3.1.slim.min.js') }}"></script>
-  <script src="{{ URL::asset('itcube/js/popper.min.js') }}"></script>
-  <script src="{{ URL::asset('itcube/js/bootstrap.min.js') }}"></script>
-  <script src="{{ URL::asset('itcube/owl-carousel/owl.carousel.min.js') }}"></script>
-  <script src="{{ URL::asset('itcube/js/main.js') }}"></script>
